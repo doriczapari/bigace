2edc56f4aeeb595fb6ebb07f80a6ea70f96f72be
Dori Czapari <dori.czapari@gmail.com>
2016-02-05 15:49:04 +0100

    Add some basic views

--- bigaceproject/views.py
+++ bigaceproject/views.py
@@ -1,3 +1,26 @@
+from bigaceproject.models import Project
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView, CreateView
+from . import models
+
+
+class ProjectListView(ListView):
+
+    template_name = 'project_list.html'
+    model = Project
+
+
+class UserListView(ListView):
+    template_name = 'user_list.html'
+    model = User
+
+
+class UserCreateView(CreateView):
+
+    template_name = 'user_create.html'
+    model = User
+    fields = ['username', 'email']
+    success_url = '/'   # FIXME

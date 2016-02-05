1773c5b2ffb5e84275dd86f61d1f50d78b3bf7b6
Dori Czapari <dori.czapari@gmail.com>
2016-02-05 12:15:11 +0100

    Add models

--- bigaceproject/models.py
+++ bigaceproject/models.py
@@ -1,3 +1,31 @@
 from django.db import models
-
 # Create your models here.
+from django.contrib.auth.models import User
+
+
+class Technology(models.Model):
+    name = models.CharField(max_length=264)
+
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User)
+    skills = models.ManyToManyField(Technology, blank=True)
+    github = models.URLField(null=True, blank=True)
+    twitter = models.CharField(max_length=100, null=True, blank=True)
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=264)
+    description = models.TextField()
+    owner = models.ForeignKey(User, related_name='owner')
+    participants = models.ManyToManyField(User, blank=True)
+    deadline = models.DateField(null=True, blank=True)
+    time = models.PositiveSmallIntegerField(null=True, blank=True)
+    technologies = models.ManyToManyField(Technology, blank=True)
+    max_people = models.PositiveSmallIntegerField(null=True, blank=True)
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=264)
+    project = models.ForeignKey(Project, related_name='tasks')
+    completed = models.BooleanField(default=False)

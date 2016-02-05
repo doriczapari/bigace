fcf61e1c4712c95ec8078231925f8a1124a4cc0b
Szilvia Kadar <kadarszilvi@gmail.com>
2016-02-06 00:15:07 +0100

    various update

--- bigaceproject/admin.py
+++ bigaceproject/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import UserProfile, Project, Task, Technology
+from .models import UserProfile, Project, Task, Technology, Rating
 
 admin.site.register(UserProfile)
 admin.site.register(Project)
 admin.site.register(Task)
 admin.site.register(Technology)
+admin.site.register(Rating)
 
 # Register your models here.
